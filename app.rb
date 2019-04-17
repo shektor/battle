39b38eb3cdf71b70f70f7320a4f4cc6980ec88b5
shektor <44544977+shektor@users.noreply.github.com>
2019-04-17 22:54:01 +0100

    Update variable names and add HP feature and test

--- app.rb
+++ app.rb
@@ -9,14 +9,14 @@ enable :sessions
   end
 
   post '/names' do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
     erb(:play)
   end
 
--- spec/features/enter_players_spec.rb
+++ spec/features/enter_players_spec.rb
@@ -1,9 +1,19 @@
 feature 'A personalised game' do
   scenario 'allow players to enter their names and see them' do
     visit '/'
-    fill_in('player1', with: 'Bob')
-    fill_in('player2', with: 'Geldof')
+    fill_in('player_1_name', with: 'Rick')
+    fill_in('player_2_name', with: 'Morty')
     click_button('Submit')
-    expect(page).to have_content 'Bob Geldof'
+    expect(page).to have_content 'Rick vs Morty'
+  end
+end
+
+feature 'How close am I to winning' do
+  scenario 'see Player 2 hit points' do
+    visit '/'
+    fill_in('player_1_name', with: 'Rick')
+    fill_in('player_2_name', with: 'Morty')
+    click_button('Submit')
+    expect(page).to have_content 'Morty (100 HP)'
   end
 end
