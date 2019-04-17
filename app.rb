c4410b32fde46d179c9702e5240cffab840fadd3
shektor <44544977+shektor@users.noreply.github.com>
2019-04-17 18:47:46 +0100

    Refactor with post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
 
+enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
