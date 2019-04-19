e7995f0675ff878854b9936c535117d2a93566b5
shektor <44544977+shektor@users.noreply.github.com>
2019-04-19 21:58:27 +0100

    Moved players to game object to reduce controller complexity

--- app.rb
+++ app.rb
@@ -5,28 +5,25 @@ require './lib/game'
 class Battle < Sinatra::Base
 
 enable :sessions
-game = Game.new
+
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(Player.new(params[:player_1_name]),Player.new(params[:player_2_name]))
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    game.attack(@player_2)
+    $game.attack($game.player_2)
+    @game = $game
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,21 @@
 require 'game'
 
 describe Game do
-  let(:player) { double(:player)}
+  let(:player_1) { double(:player) }
+  let(:player_2) { double(:player) }
+  let(:subject) { described_class.new(player_1, player_2) }
+
+  describe '#initialize' do
+    it 'assigns two players' do
+      expect(subject.player_1).to eq player_1
+      expect(subject.player_2).to eq player_2
+    end
+  end
+
   describe '#attack' do
     it 'attacks another player' do
-      expect(player).to receive(:reduce_hp)
-      subject.attack(player)
+      expect(player_1).to receive(:reduce_hp)
+      subject.attack(player_1)
     end
   end
 end
